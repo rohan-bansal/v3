78aebf8537119ea84d291781998e0f9611a2120c
Rohan Bansal <bansalrohan18@gmail.com>
2021-06-15 18:03:07 -0700

    add work section

--- js/main.js
+++ js/main.js
@@ -32,3 +32,26 @@ function triggerPops() {
     }
 
   })();
+
+  AOS.init();
+
+  AOS.init({
+    disable: 'phone',
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false, 
+    debounceDelay: 50,
+    throttleDelay: 99, 
+    
+  
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
